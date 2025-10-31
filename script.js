86df310877ca1352b3da57e8239f9987fcc836e1
krish-159 <mymothersulo@gmail.com>
2025-11-01 00:35:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let txt = 'Everything happens for a reason';
-let wCount = txt.split(' ').length;
-let replace = txt.replace('for', 'a');
-let included = txt.includes('');
-console.log(`Word count: ${wCount}`);
-console.log(`Replaced sentence: ${replace}`);
-console.log(`Contains 'fox': ${included}`);
+let txt = prompt('Enter a text with (a) included in it but before it no word should have any a in it: Challenge')
+let wCount = txt.split(' ').length
+let replace = txt.replace('a','many')
+let included = txt.includes('a')
+alert(`Word count: ${wCount}`)
+alert(`Replaced txt: ${replace}`)
+alert(`included: ${included}`)
